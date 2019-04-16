18ccb96a28a6521ce9cd3cff21c5bbc15d664977
MrJiaWenTang <1074356353@qq.com>
2019-04-16 15:57:36 +0800

    添加社区会员dao层接口

---
+++ src/main/java/com/moon/studentplatform/mapper/society/IUserClubDao.java
@@ -0,0 +1,34 @@
+package com.moon.studentplatform.mapper.society;
+
+import com.moon.studentplatform.dto.society.Club;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @author ：Mr Tang
+ * @date ：Created in 2019/4/16 15:43
+ * @description：这个类是社区会员dao层接口
+ * @modified By：
+ */
+@Repository
+@Mapper
+public interface IUserClubDao {
+    @Select("SELECT\n" +
+            "club_user.id,\n" +
+            "club.`name` as clubname,\n" +
+            "`user`.username,\n" +
+            "club_user.reason,\n" +
+            "club_user.experience,\n" +
+            "club_user.joindate,\n" +
+            "club_user.pass\n" +
+            "FROM\n" +
+            "club\n" +
+            "INNER JOIN club_user ON club_user.club_id = club.id AND '' = ''\n" +
+            "INNER JOIN `user` ON club_user.user_id = `user`.id\n" +
+            "ORDER BY\n" +
+            "club_user.pass ASC")
+    List<Club> showAllClubUsers();
+}
